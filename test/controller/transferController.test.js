cd6efb1d3713a810dce014e56c3b585a5349242f
Vanessa Santos <vanessabonne@hotmail.com>
2025-09-11 08:56:34 -0300

    adicionando um novo teste de erro na transferencia com mock

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -6,6 +6,10 @@ const {expect} = require('chai');
 // App - aplicação
 const app = require('../../app')
 
+//Mock
+const transferService = require('../../service/transferService');
+
+
 // Testes
 describe('Transfer Controller', () => {
     describe('POST / transfers', () => {
@@ -20,6 +24,27 @@ describe('Transfer Controller', () => {
 
                 expect(resposta.status).to.equal(400);
                 expect(resposta.body).to.have.property('error', 'Sender or recipient not found.');
+            });
+
+            it('Usando Mocks: Quando informo remetente e destinatário inexistentes recebo 400 e mensagem JSON', async () => {
+                // Mocar apenas a função transfer do service
+                const trasnferServiveMock = sinon.stub(transferService, 'transfer');
+                trasnferServiveMock.returns({error: 'Sender or recipient not found.'});
+                
+                
+                const resposta = await request(app)
+                    .post('/transfer')
+                    .send({
+                        from: "Vanss",
+                        to: "Priscilass",
+                        amount: 100
+                    });
+
+                expect(resposta.status).to.equal(400);
+                expect(resposta.body).to.have.property('error', 'Sender or recipient not found.');
+
+                //Reseto o Mock
+                sinon.restore()
             });
     });
 
